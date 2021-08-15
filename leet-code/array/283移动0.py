a891fc1ebda9625da06a81b0c5c09f40c6b3544e
chenxu_i <chenxu_i@didiglobal.com>
2021-08-15 22:14:19 +0800

    移动0暂定最优

---
+++ leet-code/array/283移动0.py
@@ -0,0 +1,24 @@
+# 给定一个数组 nums，编写一个函数将所有 0 移动到数组的末尾，同时保持非零元素的相对顺序。
+#
+# 示例:
+#
+# 输入: [0,1,0,3,12]
+# 输出: [1,3,12,0,0]
+class Sol(object):
+ def moveZeroes(self, nums):
+     j=0
+     for i in range(len(nums)):
+         if nums[i]!= 0:
+             nums[i],nums[j]=nums[j],nums[i]
+             j=j+1
+     return nums
+
+
+
+nums=[0,1,0,3,12]
+
+ansewr0=Sol()
+print(ansewr0.moveZeroes(nums))
+
+
+
